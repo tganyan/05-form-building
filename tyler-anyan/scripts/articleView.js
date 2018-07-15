4af4a428bfe0f94eec2f456ab41f77304e7ee705
tganyan <tganyan@gmail.com>
2018-07-15 11:35:25 -0700

    updating articleView logic, updating new.html to have article template and export template, minor update to module.css to make json preview nicer looking

--- tyler-anyan/scripts/articleView.js
+++ tyler-anyan/scripts/articleView.js
@@ -75,24 +75,24 @@ articleView.setTeasers = () => {
 };
 
 // COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// This function is called within new.html because of the differences between what gets rendered in index vs new. Document.ready will no longer suffice since we have different things happening in different contexts, so a created function needs to handle it.
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show();
 
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
 
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
   $('#new-article-form').on('change', 'input, textarea', articleView.create);
 };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
+  // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
 
   // DONE: Instantiate an article based on what's in the form fields:
@@ -105,18 +105,22 @@ articleView.create = () => {
     publishedOn: $('#article-published:checked').length ? new Date() : null,
   })
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').html(Handlebars.compile($('#article-template').text())(newArticle));
+  
 
+  // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('pre code').each((i, block) => {
+    hljs.highlightBlock(block);
+  });
 
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each();
-
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-
+  // DONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js: 
+  $('#article-export').show();
+  $('#article-export').html(Handlebars.compile($('#export-template').text())(newArticle));
 };
 
 // COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// This function is called at the bottom of new.html. This is the case because we now have two different pages that require different pieces of code to run so using document.ready is no longer viable.
 articleView.initIndexPage = () => {
   articles.forEach(article => $('#articles').append(article.toHtml()));
   articleView.populateFilters();
